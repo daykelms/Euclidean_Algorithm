7d93e11f45e3007b5365726475da808df2a39a75
Daykel Muro <37645764+daykelms@users.noreply.github.com>
2018-06-25 22:19:39 -0400

    Update Euclidean_Algorithm.java

--- Euclidean_Algorithm.java
+++ Euclidean_Algorithm.java
@@ -24,6 +24,9 @@ public class Euclidean_Algorithm {
 		int numa = kb.nextInt();
 		System.out.println("Please enter the second value");
 		int numb = kb.nextInt();
+	/* At the nth iteration the variable numb holds the latest remainder n−1, whereas the variable numa holds its predecessor n-2.
+         * At the end of the loop, the variable numb holds the remainder n, whereas the variable numa holds its predecessor, n−1.
+         */
 
 		while (numb != 0 && numa !=0 ) {
 			 
